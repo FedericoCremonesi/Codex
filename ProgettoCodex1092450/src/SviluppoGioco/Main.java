2b094e11d13f6064999b97319f4e9f44e4a527cf
feder <feder@PersonalComputerFederico.mshome.net>
2024-09-11 00:32:48 +0200

    Aggiunto Main che crea una nuova partita

---
+++ ProgettoCodex1092450/src/SviluppoGioco/Main.java
@@ -0,0 +1,12 @@
+package SviluppoGioco;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Una nuova partita ha inizio");
+		Partita partita = new Partita();
+
+	}
+
+}
