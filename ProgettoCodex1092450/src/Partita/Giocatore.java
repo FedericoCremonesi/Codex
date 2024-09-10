73e09b2e7cf0dc4e6a39fceb9fc6a009e174e3d7
feder <feder@PersonalComputerFederico.mshome.net>
2024-09-10 23:54:05 +0200

    Creata classe giocatore

---
+++ ProgettoCodex1092450/src/Partita/Giocatore.java
@@ -0,0 +1,18 @@
+package Partita;
+
+import java.util.Set;
+
+import Carte.CartaObiettivo;
+
+public class Giocatore {
+	
+	private String nickname;
+	private String colorePedina;
+	private int punti;
+	private String[][] campo;
+	private Set<String> mano;
+	private CartaObiettivo obiettivoSegreto;
+	private int conteggioObiettiviCompletati;
+	
+
+}
