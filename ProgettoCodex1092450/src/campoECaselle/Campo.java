771a6a49e79c9d54c17b5fc16fe1da97adf87097
feder <feder@PersonalComputerFederico.mshome.net>
2024-09-14 16:48:29 +0200

    Correzione nome attributo campo

--- ProgettoCodex1092450/src/campoECaselle/Campo.java
+++ ProgettoCodex1092450/src/campoECaselle/Campo.java
@@ -7,6 +7,6 @@ public class Campo {
 	 * DIM_MAX serve per costruire la matrice 81x81, dimensione massima che il campo di un giocatore può assumere
 	 * ciò si verifica se si gioca in due, si esauriscono entrambi i mazzi e il giocatore gioca le sue carte lungo una diagonale procedendo sempre nella stessa direzione
 	 */
-	private Casella[][] carteInCampo;
+	private Casella[][] caselleDelCampo;
 
 }
